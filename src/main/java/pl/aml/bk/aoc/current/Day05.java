f28061fcbeccef9629d40b9ad6848dc71de26fdc
Bartek Karaś <amalius.bk@gmail.com>
2024-12-05 20:23:19 +0100

    WIP: Refactor day 5.

--- src/main/java/pl/aml/bk/aoc/current/Day05.java
+++ src/main/java/pl/aml/bk/aoc/current/Day05.java
@@ -9,34 +9,25 @@ public class Day05 {
 
     public void part2() {
         Set<String> rules = loadRules("/inputs/day05_rules.txt");
-        List<String> pages = loadPages("/inputs/day05_pages.txt");
-        List<String> invalidPages = sortManualsByValidity(rules, pages).invalidPages;
-        List<String[]> manuals = parseManuals(invalidPages);
+        List<String> allManuals = loadManuals("/inputs/day05_manuals.txt");
+        List<String> invalidManuals = sortManualsByValidity(rules, allManuals).invalidManuals;
+        List<String[]> manuals = parseManualsToArrays(invalidManuals);
         int sum = 0;
-        // reorder
-
         for (var manual : manuals) {
             String[] rearranged = Arrays.copyOf(manual, manual.length);
             boolean isManualValid = false;
-
             do {
                 for (int i = 0; i < rearranged.length; i++) {
                     String currentPage = rearranged[i];
                     boolean swapped = false;
-                    if (rules.stream().filter(x -> x.startsWith(currentPage)).findAny().isPresent()) {
+                    if (rules.stream().anyMatch(x -> x.startsWith(currentPage))) {
                         for (int j = 0; j < rearranged.length; j++) {
-                            if (i == j) {
-                                continue;
-                            }
                             String checkedPage = rearranged[j];
-                            if (rules.contains(currentPage + "|" + checkedPage)) {
-                                boolean isPairValid = isPairValid(i, j);
-                                if (!isPairValid) {
-                                    // swap
-                                    swapPlaces(rearranged, i, j);
-                                    swapped = true;
-                                    break;
-                                }
+                            if (rules.contains(currentPage + "|" + checkedPage) && i > j) {
+                                // swap
+                                swapPlaces(rearranged, i, j);
+                                swapped = true;
+                                break;
                             }
                         }
                         if (swapped) {
@@ -46,9 +37,7 @@ public class Day05 {
                 }
                 isManualValid = checkIfManualIsValid(rules, rearranged);
             } while (!isManualValid);
-            if (isManualValid) {
-                sum += Integer.parseInt(rearranged[rearranged.length / 2]);
-            }
+            sum += Integer.parseInt(rearranged[rearranged.length / 2]);
 
         }
 
@@ -63,24 +52,22 @@ public class Day05 {
 
     public void part1() {
         Set<String> rules = loadRules("/inputs/day05_rules.txt");
-        List<String> pages = loadPages("/inputs/day05_pages.txt");
-        List<String> validPages = sortManualsByValidity(rules, pages).validPages;
+        List<String> pages = loadManuals("/inputs/day05_manuals.txt");
+        List<String> validManuals = sortManualsByValidity(rules, pages).validManuals;
         int sum = 0;
-        for (var page :
-                validPages) {
-            String[] split = page.split(",");
+        for (var manual : validManuals) {
+            String[] split = manual.split(",");
             sum += Integer.parseInt(split[split.length / 2]);
         }
 
         System.out.println(sum);
     }
 
-    private JoinedPages sortManualsByValidity(Set<String> rules, List<String> manuals) {
+    private SortedManuals sortManualsByValidity(Set<String> rules, List<String> manuals) {
         List<String> validPages = new ArrayList<>();
         List<String> invalidPages = new ArrayList<>();
         for (var manual : manuals) {
             String[] pageNumbers = manual.split(",");
-
             if (checkIfManualIsValid(rules, pageNumbers)) {
                 validPages.add(manual);
             } else {
@@ -88,47 +75,41 @@ public class Day05 {
             }
         }
 
-        return new JoinedPages(validPages, invalidPages);
-    }
-
-    private boolean isPairValid(int currentPageIdx, int checkedPageIdx) {
-        boolean isForward = false;
-        if (currentPageIdx < checkedPageIdx) {
-            isForward = true;
-        }
-        return isForward;
+        return new SortedManuals(validPages, invalidPages);
     }
 
     private boolean checkIfManualIsValid(Set<String> rules, String[] pageNumbers) {
         for (int i = 0; i < pageNumbers.length; i++) {
             String currentPage = pageNumbers[i];
-            for (int j = 0; j < pageNumbers.length; j++) {
-                boolean contains = rules.contains(currentPage + "|" + pageNumbers[j]);
-                if (!contains) {
-                    continue;
-                }
-                boolean isForward = false;
-                if (i < j) {
-                    isForward = true;
-                } else if (i == j) {
-                    continue;
-                }
+            if (rules.stream().anyMatch(x -> x.startsWith(currentPage))) {
+                for (int j = 0; j < pageNumbers.length; j++) {
+                    boolean contains = rules.contains(currentPage + "|" + pageNumbers[j]);
+                    if (!contains) {
+                        continue;
+                    }
+                    boolean isForward = false;
+                    if (i < j) {
+                        isForward = true;
+                    } else if (i == j) {
+                        continue;
+                    }
 
-                if (!isForward) {
-                    return false;
+                    if (!isForward) {
+                        return false;
+                    }
                 }
             }
         }
         return true;
     }
 
-    private List<String[]> parseManuals(List<String> pages) {
+    private List<String[]> parseManualsToArrays(List<String> pages) {
         return pages.stream()
                 .map(s -> s.split(","))
                 .toList();
     }
 
-    private List<String> loadPages(String path) {
+    private List<String> loadManuals(String path) {
         try (InputStream inputStream = Day05.class.getResourceAsStream(path)) {
             assert inputStream != null;
             return new String(inputStream.readAllBytes()).lines().toList();
@@ -147,7 +128,7 @@ public class Day05 {
         }
     }
 
-    record JoinedPages(List<String> validPages, List<String> invalidPages) {
+    record SortedManuals(List<String> validManuals, List<String> invalidManuals) {
     }
 
 }
